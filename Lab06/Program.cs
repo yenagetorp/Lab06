bddd2f475492f4c2b2aabe8faa457197fcf2ca08
Johan Nensen <jnensen@hotmail.com>
2018-11-02 13:20:45 +0100

    Revert "Revert "lade til ett namn""
    This reverts commit 4bd557b474dadae8e958ba696fa7deea85df4953.

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -21,7 +21,7 @@ namespace RpgAppLab3
             if (command == "names")
             {
 
-                var allNames = new[] { "Ben", "David", "Dana", "Felix", "Elrond" };
+                var allNames = new[] { "Ben", "David", "Dana", "Felix", "Elrond", "Elrond den andre" };
 
 
                 var randomIndex = rnd.Next(allNames.Length);
