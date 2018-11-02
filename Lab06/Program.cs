2f4031f50663ec2c6f159e272e65a5b21c854ecb
Cengizhan <gengishan1400@gmail.com>
2018-11-02 11:42:41 +0100

    Skriver igen

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Lab06
+namespace RpgAppLab3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rnd = new Random();
+            Console.WriteLine("What do you need?");
+            var command = Console.ReadLine();
+            if (command == "roll d20")
+            {
+                var diceRoll = rnd.Next(20) + 1;
+                Console.WriteLine($"your dice roll was: {diceRoll}");
+            }
+            if (command == "names")
+            {
+
+                var allNames = new[] { "Ben", "David", "Dana", "Felix", "Elrond" };
+
+                // كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟ كيف حالك ؟
+
+                var randomIndex = rnd.Next(allNames.Length);
+                Console.WriteLine(allNames[randomIndex]);
+            }
+            if (command == "Tolkien names")
+            {
+                var tolkienNames = new[] { "legolas", "Elrond", "Frodo", "Aragaron", "Gandalf" };
+                var randomIndex = rnd.Next(tolkienNames.Length);
+                Console.WriteLine(tolkienNames[randomIndex]);
+            }
+            Console.ReadKey();
         }
     }
 }
